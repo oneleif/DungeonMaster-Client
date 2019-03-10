f0c5bb51e02d15310d02d4e37b3b1101f2d406ef
ambid17 <sambrose@nebrwesleyan.edu>
2019-03-09 22:25:41 -0600

    Added character pulling to dev console, made it generic

---
+++ DungeonMaster/Assets/Scripts/Models/Ability.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Ability
+{
+    [SerializeField]
+    int damageBonus;
+    [SerializeField]
+    string damageDice;
+    [SerializeField]
+    int attackBonus;
+    [SerializeField]
+    string description;
+    [SerializeField]
+    string name;
+
+    public Ability(int damageBonus, string damageDice, int attackBonus, string description, string name) {
+        this.damageBonus = damageBonus;
+        this.damageDice = damageDice;
+        this.attackBonus = attackBonus;
+        this.description = description;
+        this.name = name;
+    }
+}
