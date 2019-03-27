07c890c5811ab8a75cdb8ed0a1537db1c2796aa3
ambid17 <sambrose@nebrwesleyan.edu>
2019-03-26 21:55:18 -0500

    Drawing working, needs to be integrated with view controllers

--- DungeonMaster/Assets/Scripts/DrawViewController.cs
+++ DungeonMaster/Assets/Scripts/DrawViewController.cs
@@ -1,22 +1,21 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(Collider2D))]  // REQUIRES A COLLIDER2D to function
+// REQUIRES A COLLIDER2D to function
 // 1. Attach this to a read/write enabled sprite image
 // 2. Set the drawing_layers  to use in the raycast
 // 3. Attach a 2D collider (like a Box Collider 2D) to this sprite
 // 4. Hold down left mouse to draw on this texture!
-public class DrawViewController : MonoBehaviour {
+public class DrawViewController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
     DrawViewModel drawViewModel;
 
     public delegate void Brush_Function(Vector2 worldPosition);
     public Brush_Function currentBrush;
 
-    public LayerMask drawLayerMask;
-
     // MUST HAVE READ/WRITE enabled set in the file editor of Unity
+    Image drawImage;
     Sprite drawSprite;
     Texture2D drawTexture;
 
@@ -27,15 +26,14 @@ public class DrawViewController : MonoBehaviour {
 
     Color32[] currentColors;
 
-    bool mouseWasHeldDown = false;
-    bool isNotDrawingOnCurrentDrag = false;
+    RectTransform rectTransform;
 
     void Awake() {
         currentBrush = PenBrush;
 
-        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-        drawSprite = renderer.sprite;
-        renderer.material.renderQueue = 3001;
+        rectTransform = GetComponent<RectTransform>();
+        drawImage = GetComponent<Image>();
+        drawSprite = drawImage.sprite;
         drawTexture = drawSprite.texture;
 
         drawViewModel = new DrawViewModel();
@@ -50,44 +48,29 @@ public class DrawViewController : MonoBehaviour {
     }
 
     void Update() {
-        MouseInput();
         KeyboardInput();
     }
 
-    void MouseInput() {
-        bool isMouseHeldDown = Input.GetMouseButton(0);
-        if (isMouseHeldDown && !isNotDrawingOnCurrentDrag) {
-            // Convert mouse coordinates to world coordinates
-            Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // Check if the current mouse position overlaps our image
-            Collider2D hit = Physics2D.OverlapPoint(mouseWorldPosition);
-            if (hit != null && hit.transform != null) {
-                if (!mouseWasHeldDown) {
-                    drawViewModel.AddUndo(drawTexture.GetPixels32());
-                }
-                // We're over the texture we're drawing on!
-                // Use whatever function the current brush is
-                currentBrush(mouseWorldPosition);
-            } else {
-                // We're not over our destination texture
-                previousDragPosition = Vector2.zero;
-                if (!mouseWasHeldDown) {
-                    // This is a new drag where the user is left clicking off the canvas
-                    // Ensure no drawing happens until a new drag is started
-                    isNotDrawingOnCurrentDrag = true;
-                }
+    void KeyboardInput() {
+#if UNITY_EDITOR
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            && Input.GetKeyDown(KeyCode.Z)) {
+            Debug.Log("x");
+            if (drawViewModel.CanUndo()) {
+                Debug.Log("y");
+                currentColors = drawViewModel.Undo(drawTexture.GetPixels32());
+                ApplyMarkedPixelChanges();
             }
         }
-        // Mouse is released
-        else if (!isMouseHeldDown) {
-            previousDragPosition = Vector2.zero;
-            isNotDrawingOnCurrentDrag = false;
-        }
-        mouseWasHeldDown = isMouseHeldDown;
-    }
 
-    void KeyboardInput() {
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            && Input.GetKeyDown(KeyCode.Y)) {
+            if (drawViewModel.CanRedo()) {
+                currentColors = drawViewModel.Redo(drawTexture.GetPixels32());
+                ApplyMarkedPixelChanges();
+            }
+        }
+#else
         if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
             && Input.GetKeyDown(KeyCode.Z)) {
             if (Input.GetKey(KeyCode.LeftShift) && drawViewModel.CanRedo()) {
@@ -98,14 +81,14 @@ public class DrawViewController : MonoBehaviour {
                 ApplyMarkedPixelChanges();
             }
         }
+#endif
     }
+
 
     // Pass in a point in WORLD coordinates
     // Changes the surrounding pixels of the world_point to the pen_colour
-    public void PenBrush(Vector2 worldPoint) {
-        // 1. Change world position to pixel coordinates
-        Vector2 pixelPosition = WorldToPixelCoordinates(worldPoint);
-        // 2. Make sure our variable for pixel array is updated in this frame
+    public void PenBrush(Vector2 pixelPosition) {
+        //Make sure our variable for pixel array is updated in this frame
         currentColors = drawTexture.GetPixels32();
 
         if (previousDragPosition == Vector2.zero) {
@@ -166,6 +149,7 @@ public class DrawViewController : MonoBehaviour {
 
         currentColors[array_pos] = color;
     }
+
     public void ApplyMarkedPixelChanges() {
         drawTexture.SetPixels32(currentColors);
         drawTexture.Apply();
@@ -190,30 +174,35 @@ public class DrawViewController : MonoBehaviour {
         drawTexture.Apply();
     }
 
-
-    public Vector2 WorldToPixelCoordinates(Vector2 world_position) {
-        // Change coordinates to local coordinates of this image
-        Vector3 local_pos = transform.InverseTransformPoint(world_position);
-
-        // Change these to coordinates of pixels
-        float pixelWidth = drawSprite.rect.width;
-        float pixelHeight = drawSprite.rect.height;
-        float unitsToPixels = pixelWidth / drawSprite.bounds.size.x * transform.localScale.x;
-
-        // Need to center our coordinates
-        float centered_x = local_pos.x * unitsToPixels + pixelWidth / 2;
-        float centered_y = local_pos.y * unitsToPixels + pixelHeight / 2;
-
-        // Round current mouse position to nearest pixel
-        Vector2 pixel_pos = new Vector2(Mathf.RoundToInt(centered_x), Mathf.RoundToInt(centered_y));
-
-        return pixel_pos;
-    }
-
-
     // Changes every pixel to be the reset colour
     public void ResetCanvas() {
         drawTexture.SetPixels(resetColorsArray);
         drawTexture.Apply();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        drawViewModel.AddUndo(drawTexture.GetPixels32());
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Vector2 localCursor = Vector2.zero;
+        if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            rectTransform, eventData.position, eventData.pressEventCamera, out localCursor)) {
+            return;
+        }
+
+        //Check if the cursor is in the bounds of the image
+        //Pivot MUST be 0,0
+        if (Mathf.Abs(localCursor.x) < drawImage.sprite.rect.width && Mathf.Abs(localCursor.y) < drawImage.sprite.rect.height) {
+            float rectToPixelScale = drawImage.sprite.rect.width / rectTransform.rect.width;
+            localCursor = new Vector2(localCursor.x * rectToPixelScale, localCursor.y * rectToPixelScale);
+            currentBrush(localCursor);
+        }
+
+        previousDragPosition = localCursor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        previousDragPosition = Vector2.zero;
     }
 }
